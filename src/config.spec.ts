f3b23300b16f7498bd8d0d8c5eab1e10c67acb5e
Alan Agius <alan.agius4@gmail.com>
2017-04-01 16:53:18 +0200

    Revert "first commit"
    This reverts commit d69fa30ef14ada082b502e5c6be3cb51a9dbf7bb.

--- src/config.spec.ts
+++ src/config.spec.ts
@@ -1,24 +1,12 @@
 import * as mockFs from "mock-fs";
 
-import { json } from "./json";
+import { config } from "./config";
 
-describe("jsonSpec", () => {
-	describe(json.read.name, () => {
+describe("fileSystemSpec", () => {
+	describe(config.readConfigFile.name, () => {
 		const MAIN_FILE = {
 			extends: [
 				"./config/primary-extend.json"
-			],
-			rules: {
-				message: {
-					maxLength: 100,
-					minLength: 2
-				}
-			}
-		};
-
-		const MAIN_FILE_WITH_ALIAS = {
-			extends: [
-				"primary-extend"
 			],
 			rules: {
 				message: {
@@ -50,7 +38,6 @@ describe("jsonSpec", () => {
 		beforeEach(() => {
 			mockFs({
 				"speedy-commit-msg-hook.json": JSON.stringify(MAIN_FILE),
-				"speedy-commit-msg-hook-alias.json": JSON.stringify(MAIN_FILE_WITH_ALIAS),
 				config: {
 					"primary-extend.json": JSON.stringify(PRIMARY_EXTEND_FILE),
 					"secondary-extend.json": JSON.stringify(SECONDARY_EXTEND_FILE)
@@ -62,31 +49,21 @@ describe("jsonSpec", () => {
 			mockFs.restore();
 		});
 
-
-		it("should merge object of extended config with alias", async done => {
-			const result = await json.read<any>("speedy-commit-msg-hook-alias.json", {
-				"primary-extend": "./config/primary-extend.json"
-			});
-			expect(result.rules.message.maxLength).toBe(100);
-			expect(result.rules.message.noDashes).toBe(true);
-			done();
-		});
-
 		it("should merge object of extended config", async done => {
-			const result = await json.read<any>("speedy-commit-msg-hook.json");
+			const result = await config.readConfigFile<any>("speedy-commit-msg-hook.json");
 			expect(result.rules.message.maxLength).toBe(100);
 			expect(result.rules.message.noDashes).toBe(true);
 			done();
 		});
 
 		it("should override same properties", async done => {
-			const result = await json.read<any>("speedy-commit-msg-hook.json");
+			const result = await config.readConfigFile<any>("speedy-commit-msg-hook.json");
 			expect(result.rules.message.minLength).toBe(2);
 			done();
 		});
 
 		it("should merge properties of nested extended config", async done => {
-			const result = await json.read<any>("speedy-commit-msg-hook.json");
+			const result = await config.readConfigFile<any>("speedy-commit-msg-hook.json");
 			expect(result.rules.message.noDashes).toBe(true);
 			expect(result.rules.message.minLength).toBe(2);
 			expect(result.rules.message.noWhiteSpace).toBe(true);
---
+++ src/package.ts
@@ -0,0 +1,15 @@
+import { readFileSync } from "fs";
+
+export namespace packageJson {
+
+	/**
+	 * Get the version number from a `package.json`
+	 *
+	 * @param {string} [packagePath="./package.json"]
+	 * @returns {string}
+	 */
+	export function getVersion(packagePath = "./package.json"): string {
+		return JSON.parse(readFileSync(packagePath, "utf-8")).version || "";
+	}
+
+}
